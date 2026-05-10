77b070d259c28da63f24577c07efe65a553612f4
rong-hue <rong-hue@users.noreply.github.com>
2026-05-10 21:25:55 +0800

    fix: use absolute inset-y-0 for TalismanPanel to bypass flex height chain

--- src/app/[locale]/studio/page.tsx
+++ src/app/[locale]/studio/page.tsx
@@ -470,7 +470,7 @@ function StudioContent() {
           }}
         />
 
-        <div className="flex-1 flex overflow-hidden min-h-0">
+        <div className="flex-1 flex overflow-hidden min-h-0 relative">
         {/* 印章面板 */}
         {showStampPanel && (
           <StampPanel
